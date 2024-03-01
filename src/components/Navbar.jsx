8ca426a70e624807a4394261b6149bc17ebc97dd
Ebenezer <ebenirene5@gmail.com>
2024-03-01 22:49:27 +0100

    resolved tailwind dependency issue

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <div className="flex justify-between items-center w-full h-20 px-4 bg-black text-white fixed">
+        <h1 className="text-5xl font-signature ml-2">Etechub</h1>
+      </div>
+    </div>
+  );
+};
