15894d5f01c24fbfa1f3ce7dbf1d0eb929f475bc
Ebenezer <ebenirene5@gmail.com>
2024-03-02 15:38:05 +0100

    updated the home and about components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,23 +13,13 @@ const About = () => {
           </p>
         </div>
         <p className="text-xl mt-20">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-          qui fugit numquam! Cum vitae temporibus molestiae dicta illum
-          laboriosam similique at mollitia nihil iusto repellat numquam eos illo
-          perspiciatis a unde minima commodi id cupiditate laborum iste, beatae
-          maiores. Voluptatum quisquam dignissimos tempore asperiores sit
-          ratione officiis officia alias vitae.
+        I'm Ebenezer, a passionate full-stack developer with a love for crafting innovative solutions and bringing ideas to life. I've honed my skills in both frontend and backend development, mastering technologies like React, Tailwind, GraphQL, Java SpringBoot and MongoDB along with other notable technologies. Whether it's building responsive user interfaces or architecting robust backend systems, I thrive on the challenges of software development and constantly seek opportunities to expand my knowledge
         </p>
 
         <br />
 
         <p className="text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-          qui fugit numquam! Cum vitae temporibus molestiae dicta illum
-          laboriosam similique at mollitia nihil iusto repellat numquam eos illo
-          perspiciatis a unde minima commodi id cupiditate laborum iste, beatae
-          maiores. Voluptatum quisquam dignissimos tempore asperiores sit
-          ratione officiis officia alias vitae.
+        Driven by a relentless curiosity and a dedication to excellence, I'm committed to delivering high-quality solutions that exceed expectations. I believe in the power of technology to make a positive impact on the world, and I'm excited to be part of an industry that's constantly pushing the boundaries of what's possible. When I'm not coding, you can find me exploring new technologies, collaborating with fellow developers, or tinkering with side projects. Let's connect and embark on a journey of innovation together!
         </p>
       </div>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,12 +12,17 @@ const Home = () => {
       <div className=" max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm Ebenezer Irene and I'm a Full Stack Developer
+            Hello! I'm Ebenezer Irene and I'm a Full Stack Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I have 8 years of experience building and desgining software.
-            Currently, I love to work on web application using technologies like
-            React, Tailwind, Next JS and GraphQL.
+            welcome to my corner of the web! I'm delighted to have you here and
+            I'm thrilled to share my journey with you. Join me as we explore the
+            fascinating world of full-stack development, from frontend marvels
+            to backend wonders. Dive into my projects, discover my experiences,
+            and let's embark on this exciting journey together. Feel free to
+            reach out and connect. 
+            Thank you for stopping
+            by, and I hope you enjoy your stay!
           </p>
           <div>
             <Link
