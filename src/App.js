439e9e75e9b37824cfe22eeb991fa35a32501266
Ebenezer <ebenirene5@gmail.com>
2024-03-02 13:40:40 +0100

    added and styled, about, portfolio and experience component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Navbar } from "./components/Navbar";
 import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <Navbar />
       <Home />
+      <About />
+      <Portfolio />
       <SocialLinks />
     </div>
   );
