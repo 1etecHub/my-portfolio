fb7fff390fe1a708686a13121916b65f75c4088e
Ebenezer <ebenirene5@gmail.com>
2024-03-05 13:32:24 +0100

    resolved responsiveness issue

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -92,7 +92,7 @@ const Experience = () => {
   return (
     <div
       name="experience"
-      className="bg-gradient-to-b from-gray-800 to-black w-full mb-40"
+      className="bg-gradient-to-b from-gray-800 to-black w-full pb-40"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
