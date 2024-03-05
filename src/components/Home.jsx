08fbb8fd437618c1012e48c6f97c210be9709684
Ebenezer <ebenirene5@gmail.com>
2024-03-05 13:41:43 +0100

    resolved responsiveness issue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -42,7 +42,7 @@ const Home = () => {
           <img
             src={Ebenezer}
             alt="my profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full"
+            className="rounded-2xl mx-auto md:w-full"
           />
         </div>
       </div>
